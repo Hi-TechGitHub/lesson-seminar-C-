4322d04f6c188228a89f93f8612d3ba3c13d7795
Hi-TechGitHub <116220279+Hi-TechGitHub@users.noreply.github.com>
2022-11-10 20:16:19 +0300

    error

--- example001/Program.cs
+++ example001/Program.cs
@@ -1,8 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! I am love dotnet!");
-string Username = Console.ReadLine();
-if (Username.ToLower() == "Вова" ) {
-    System.Console.WriteLine("УРААА");
+﻿Console.Clear();
 
-}
-System.Console.WriteLine("Привет " + Username);
+int xa = 1, ya = 1, xb = 1, yb = 30, xc = 80, yc = 40;
+
+Console.SetCursorPosition(xa, ya);
+System.Console.WriteLine("+");
+Console.SetCursorPosition(xb, yb);
+System.Console.WriteLine("+");
+Console.SetCursorPosition(xc, yc);
+System.Console.WriteLine("+");
+int x = xa, y = xb;
+int count = 0;
+while (count <10) {
+    int what = new Random().Next(0, 3);
+    if (what == 0) {
+        x = (x + xa)/2;
+        y = (y + ya)/2;
+    }
+    if (what == 1) {
+        x = (x + xb)/2;
+        y = (y + yb)/2;
+    }
+    if (what == 2) {
+        x = (x + xc)/2;
+        y = (y + yc)/2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count += 1;
+
+} 
