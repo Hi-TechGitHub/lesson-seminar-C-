421260c37d89fefd73b60ac80352412ecd5aff10
Hi-TechGitHub <116220279+Hi-TechGitHub@users.noreply.github.com>
2022-11-10 19:39:35 +0300

    name

--- example001/Program.cs
+++ example001/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! I am love dotnet!");
+string Username = Console.ReadLine();
+if (Username.ToLower() == "Вова" ) {
+    System.Console.WriteLine("УРААА");
 
+}
+System.Console.WriteLine("Привет " + Username);
